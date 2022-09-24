79b19d5171e157cd90112ba98420ccb4359a2e53
Kevin Alessi <minicarota15@gmail.com>
2022-09-24 15:59:38 +0200

    formatted

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,4 @@ module.exports = {
     "^.+\\.ts?$": "ts-jest",
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
-/*   clearMocks: true,
-  setupFilesAfterEnv:  ["./src/lib/prisma/client.mock.ts"] */
 };
